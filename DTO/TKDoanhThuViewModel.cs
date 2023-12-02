1e63958d7bbb20edf8a67f0223f670d36675fc19
taichungphat <kimanh300516@gmail.com>
2023-12-02 10:52:10 +0700

    update

---
+++ DTO/TKDoanhThuViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class TKDoanhThuViewModel
+    {
+        public TKDoanhThuViewModel(DateTime ngay, int sLDonHang, double doanhThu)
+        {
+            Ngay = ngay;
+            SLDonHang = sLDonHang;
+            DoanhThu = doanhThu;
+        }
+
+        public DateTime Ngay { get; set; }
+        public int SLDonHang { get; set; } = 0;
+        public double DoanhThu { get; set; } = 0;
+    }
+}
